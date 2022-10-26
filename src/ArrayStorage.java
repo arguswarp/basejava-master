25658e36b431adde9ba8d8583f1f1d3b8fe411f6
ArgusWarp <arguswarp@gmail.com>
2022-10-26 14:55:34 +0300

    all methods complete in Array Storage seems working

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -7,6 +9,10 @@ public class ArrayStorage {
     private int size=0;
 
     void clear() {
+        for (int i=0; i < size; i++) {
+            storage[i] = null;
+        }
+        size =0;
     }
 
     void save(Resume r) {
@@ -20,21 +26,42 @@ public class ArrayStorage {
                 return storage[i];
             }
         }
+      //  System.out.println("No such uuid in the Storage");
         return null;
     }
 
     void delete(String uuid) {
+        if (get(uuid) != null) {
+            int index = -1;
+            for (int i = 0; i < size; i++) {
+                if (storage[i].uuid.equals(uuid)) {
+                    storage[i] = null;
+                    index =i;
+                    size--;
+                }
+            }
+            for (int i =0; i <size; i++ ) {
+                if (i >= index) {
+                    storage[i] = storage [i+1];
+                }
+            }
+            storage[size+1] = null;
+        } else {
+            System.out.println("No such uuid in the Storage");
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        if ( size == 0) System.out.println("Storage is empty");
+        return Arrays.copyOfRange(storage,0,size);
     }
 
     int size() {
 
         return size;
     }
+
 }
