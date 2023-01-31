d30880d41a5ad14fba903cdc46568183e87d7550
ArgusWarp <arguswarp@gmail.com>
2023-01-31 23:29:30 +0300

    code style check before sending

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -6,13 +6,13 @@ import java.util.Arrays;
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
-    private int size=0;
+    private int size = 0;
 
     void clear() {
-        for (int i=0; i < size; i++) {
+        for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
-        size =0;
+        size = 0;
     }
 
     void save(Resume r) {
@@ -21,12 +21,12 @@ public class ArrayStorage {
     }
 
     Resume get(String uuid) {
-        for (int i=0; i < size; i++) {
-            if (storage[i].uuid.equals(uuid)){
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
                 return storage[i];
             }
         }
-      //  System.out.println("No such uuid in the Storage");
+        //  System.out.println("No such uuid in the Storage");
         return null;
     }
 
@@ -36,16 +36,16 @@ public class ArrayStorage {
             for (int i = 0; i < size; i++) {
                 if (storage[i].uuid.equals(uuid)) {
                     storage[i] = null;
-                    index =i;
+                    index = i;
                     size--;
                 }
             }
-            for (int i =0; i <size; i++ ) {
+            for (int i = 0; i < size; i++) {
                 if (i >= index) {
-                    storage[i] = storage [i+1];
+                    storage[i] = storage[i + 1];
                 }
             }
-            storage[size+1] = null;
+            storage[size + 1] = null;
         } else {
             System.out.println("No such uuid in the Storage");
         }
@@ -55,8 +55,10 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        if ( size == 0) System.out.println("Storage is empty");
-        return Arrays.copyOfRange(storage,0,size);
+        if (size == 0) {
+            System.out.println("Storage is empty");
+        }
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
     int size() {
