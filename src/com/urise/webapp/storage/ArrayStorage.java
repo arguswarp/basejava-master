19e81b56c212ff3c39bdc64e2269f21c536a2f62
ArgusWarp <arguswarp@gmail.com>
2023-02-06 23:52:44 +0300

    new package structure,refactored ArrayStorage

---
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -0,0 +1,78 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
+import java.util.Arrays;
+
+/**
+ * Array based storage for Resumes
+ */
+public class ArrayStorage {
+    private Resume[] storage = new Resume[10000];
+
+    private int size = 0;
+
+    public void clear() {
+        Arrays.fill(storage, 0, size-1, null);
+        size = 0;
+    }
+
+    public void save(Resume resume) {
+        int index = getIndex(resume.getUuid());
+        if (index == -1) {
+            storage[size++] = resume;
+        } else {
+            System.out.println("ERROR : resume " + resume.getUuid() + " is already in the storage");
+        }
+    }
+
+    public Resume get(String uuid) {
+        int index = getIndex(uuid);
+        if (index > -1) {
+            return storage[index];
+        } else {
+            System.out.println("ERROR : no such uuid " + uuid + " in the storage");
+            return null;
+        }
+    }
+
+    public void delete(String uuid) {
+        int index = getIndex(uuid);
+        if (index > -1) {
+            storage[index] = null;
+            size--;
+            System.arraycopy(storage, index + 1, storage, index, size - index);
+        } else {
+            System.out.println("ERROR : no such uuid " + uuid + " in the storage");
+        }
+    }
+
+    public void update(Resume resume) {
+        int index = getIndex(resume.getUuid());
+        if (index > -1) {
+            storage[index] = resume;
+        } else {
+            System.out.println("ERROR : resume " + resume.getUuid() + " is not present in the storage");
+        }
+    }
+
+    private int getIndex(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    public Resume[] getAll() {
+        return Arrays.copyOf(storage, size);
+    }
+
+    public int size() {
+        return size;
+    }
+}
