5ac99c25078269417a6ef653c84a8059ba49459d
ArgusWarp <arguswarp@gmail.com>
2023-03-02 20:15:28 +0300

    AbstractStorageTest implementation

---
+++ test/ru/javawebinar/basejava/storage/AbstractStorageTest.java
@@ -0,0 +1,109 @@
+package ru.javawebinar.basejava.storage;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.javawebinar.basejava.exception.ExistStorageException;
+import ru.javawebinar.basejava.exception.NotExistStorageException;
+import ru.javawebinar.basejava.model.Resume;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public abstract class AbstractStorageTest {
+    private final Storage storage;
+    private static final String UUID_1 = "uuid1";
+    private static final String UUID_2 = "uuid2";
+    private static final String UUID_3 = "uuid3";
+    private static final String UUID_SAVED = "saved";
+    private static final String UUID_NOT_EXIST = "dummy";
+    private final Resume RESUME_1 = new Resume(UUID_1);
+    private final Resume RESUME_2 = new Resume(UUID_2);
+    private final Resume RESUME_3 = new Resume(UUID_3);
+    private final Resume RESUME_SAVED = new Resume(UUID_SAVED);
+
+    protected AbstractStorageTest(Storage storage) {
+        this.storage = storage;
+    }
+
+    @BeforeEach
+    public void setUp() {
+        storage.clear();
+        storage.save(RESUME_1);
+        storage.save(RESUME_2);
+        storage.save(RESUME_3);
+    }
+
+    @Test
+    public void clear() {
+        storage.clear();
+        assertSize(0);
+        Resume[] emptyStorage = new Resume[0];
+        assertArrayEquals(emptyStorage, storage.getAll());
+    }
+
+    @Test
+    public void get() {
+        assertGet(RESUME_1);
+        assertGet(RESUME_2);
+        assertGet(RESUME_3);
+    }
+
+    private void assertGet(Resume expectedResume) {
+        String uuid = expectedResume.getUuid();
+        assertEquals(expectedResume, storage.get(uuid));
+    }
+
+    @Test
+    public void getNotExist() {
+        assertThrows(NotExistStorageException.class, () -> storage.get(UUID_NOT_EXIST));
+    }
+
+    @Test
+    public void save() {
+        storage.save(RESUME_SAVED);
+        assertGet(RESUME_SAVED);
+        assertSize(4);
+    }
+
+    @Test
+    public void saveExist() {
+        assertThrows(ExistStorageException.class, () -> storage.save(new Resume(UUID_1)));
+    }
+
+    @Test
+    public void delete() {
+        storage.delete(UUID_2);
+        assertSize(2);
+        assertThrows(NotExistStorageException.class, () -> storage.get(UUID_2));
+    }
+
+    @Test
+    public void deleteNotExist() {
+        assertThrows(NotExistStorageException.class, () -> storage.delete(UUID_NOT_EXIST));
+    }
+
+    @Test
+    public void update() {
+        storage.update(RESUME_3);
+        assertSame(RESUME_3, storage.get(UUID_3));
+    }
+
+    @Test
+    public void updateNotExist() {
+        assertThrows(NotExistStorageException.class, () -> storage.update(new Resume(UUID_NOT_EXIST)));
+    }
+
+    @Test
+    public void getAll() {
+        Resume[] resumes = {new Resume(UUID_1), new Resume(UUID_2), new Resume(UUID_3)};
+        assertArrayEquals(resumes, storage.getAll());
+    }
+
+    @Test
+    public void size() {
+        assertSize(3);
+    }
+
+    private void assertSize(int expected) {
+        assertEquals(expected, storage.size());
+    }
+}
---
+++ test/ru/javawebinar/basejava/storage/ListStorageTest.java
@@ -0,0 +1,10 @@
+package ru.javawebinar.basejava.storage;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ListStorageTest extends AbstractStorageTest {
+
+    public ListStorageTest() {
+        super(new ListStorage());
+    }
+}
