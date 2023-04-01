ebd6df4209d9f1be6e4f6939b5a856bb85e2554a
ArgusWarp <arguswarp@gmail.com>
2023-04-01 23:53:25 +0300

    recursive folder traversal method in MainFile

---
+++ src/ru/javawebinar/basejava/MainFile.java
@@ -0,0 +1,29 @@
+package ru.javawebinar.basejava;
+
+import java.io.File;
+import java.util.Optional;
+
+public class MainFile {
+    public static void main(String[] args) {
+        File directory = new File("./");
+
+        System.out.println(directory.isDirectory());
+        System.out.println(directory.getAbsolutePath());
+        System.out.println();
+
+        showFiles(directory);
+        System.out.println();
+    }
+    public static void showFiles(File directory) {
+        Optional<File[]> files = Optional.ofNullable(directory.listFiles());
+        if (files.isPresent()) {
+            for (File f : files.get()) {
+                if (f.isFile()) {
+                    System.out.println(f.getName());
+                } else {
+                    showFiles(f);
+                }
+            }
+        }
+    }
+}
