63d32cc247cdc738f47fa7939857cc2e6931c86c
ArgusWarp <arguswarp@gmail.com>
2023-05-18 23:43:28 +0300

    crud view sections with HtmlUtil class

--- src/ru/javawebinar/basejava/util/HtmlUtil.java
+++ src/ru/javawebinar/basejava/util/HtmlUtil.java
@@ -2,30 +2,47 @@ package ru.javawebinar.basejava.util;
 
 import ru.javawebinar.basejava.model.*;
 
+import java.util.Optional;
+
 public class HtmlUtil {
     public static String sectionToHtml(SectionType type, AbstractSection section) {
         StringBuilder message = new StringBuilder();
+        message.append("<br/>");
         switch (type) {
             case PERSONAL, OBJECTIVE -> {
                 TextSection textSection = (TextSection) section;
-                message.append(textSection.getContent());
+                message.append("<br/>")
+                        .append(textSection.getContent())
+                        .append("</br>");
             }
             case ACHIEVEMENT, QUALIFICATIONS -> {
                 ListSection listSection = (ListSection) section;
-                message.append(String.join("<br/>", listSection.getItems()));
+                message.append("<br/>")
+                        .append(String.join("<br/>", listSection.getItems()))
+                        .append("</br>");
             }
             case EXPERIENCE, EDUCATION -> {
                 CompanySection companySection = (CompanySection) section;
                 for (Company c : companySection.getCompanies()) {
-                    message.append(c.getHomepage().getName())
-                            .append("</br>")
-                            .append(c.getHomepage().getUrl())
+                    Optional<String> url = Optional.ofNullable(c.getHomepage().getUrl());
+                    message.append("</br>")
+                            .append(c.getHomepage().getName())
+                            .append(url.map(s -> "</br>").orElse(""))
+                            .append(url.map(s -> "<a href ='" + s + "'>" + s + "</a>").orElse(""))
                             .append("</br>");
-                    c.getPeriods().forEach(period -> message.append(period.getTitle()).append(period.getDescription()));
+                    c.getPeriods().forEach(period -> {
+                        Optional<String> description = Optional.ofNullable(period.getDescription());
+                        message.append(period.getStartDate()).append(" - ").append(period.getEndDate())
+                                .append(" ")
+                                .append(period.getTitle())
+                                .append(description.map(s ->"</br>" ).orElse(""))
+                                .append(description.orElse(""))
+                                .append("</br>");
+                    });
                 }
             }
             default -> throw new IllegalStateException("Section type is invalid");
         }
-        return "<b>" + type.getTitle() + "</b>" + ":" + "<br/>" + message + "<br/>";
+        return "<b>" + type.getTitle() + "</b>" + ":" + message;
     }
 }
